bf13daf391a063357abc5d2ba032bb8fd9f61e8a
Peyangpiece <150012568+Peyangpiece@users.noreply.github.com>
2026-08-26 06:19:47 +0900

    Connect standalone Live Gate to Firebase project

---
+++ livegate-prod/public/firebase-config.js
@@ -0,0 +1,9 @@
+window.LIVEGATE_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAanVETIredUVH1slS8OtIMSPdOn91u2HM",
+  authDomain: "ballpuzzle-8cc87.firebaseapp.com",
+  databaseURL: "https://ballpuzzle-8cc87-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "ballpuzzle-8cc87",
+  storageBucket: "ballpuzzle-8cc87.firebasestorage.app",
+  messagingSenderId: "114162791129",
+  appId: "1:114162791129:web:14a0fc04ca3c6f5d9f84c8"
+};
